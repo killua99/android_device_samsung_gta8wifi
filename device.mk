2cb741403621e5268290274d55691b44770d15bc
Luigi Guevara <luigi.fox15@gmail.com>
2025-09-03 22:18:38 +0200

    New samsung device

---
+++ device.mk
@@ -0,0 +1,41 @@
+#
+# Copyright (C) 2025 The LineageOS Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# Get non-open-source specific aspects
+$(call inherit-product, vendor/samsung/gta8wifi/gta8wifi-vendor.mk)
+
+# Local overlays
+DEVICE_PACKAGE_OVERLAYS += \
+    $(LOCAL_PATH)/overlay
+
+# Rotation
+PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
+    ro.bootanim.set_orientation_4630946474265338241=ORIENTATION_90
+
+# Device init scripts
+PRODUCT_PACKAGES += \
+    init.gta8wifi.rc
+
+# Soong namespaces
+PRODUCT_SOONG_NAMESPACES += \
+    $(LOCAL_PATH)
+
+# WiFi firmware
+PRODUCT_PACKAGES += \
+    dummy_wlanmdsp.mbn
+
+# Inherit from gta8-common
+$(call inherit-product, device/samsung/gta8-common/gta8.mk)
